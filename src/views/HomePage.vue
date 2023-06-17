0515db2e649ebc8bcafcdd9e6312e9a0657306d6
solar <solarinius@gmail.com>
2023-06-17 17:49:33 +0300

    add corsproxy, start 1 min poll cycle

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -18,101 +18,45 @@ export default defineComponent({
   }),
 
   computed: {
-    currentDate() {
-      return this.today.getDate();
-    },
 
-    currentHour() {
-      return this.today.getHours();
-    },
-
-    hourToEraseCurrentAdvice() {
-      let oneHourPrior = this.hourToFetchNewAdvice - 1;
-      if (oneHourPrior < 0) oneHourPrior = 23;
-      return oneHourPrior;
-    },
   },
 
   // When we'll check if advice data needs to be changed
   async ionViewWillEnter() {
-    // If we haven't stored an hourToFetchNewAdvice before, calculate and store that and hourToEraseCurrentAdvice
-    if (!this.hourToFetchNewAdvice) this.hourToFetchNewAdvice = this.currentHour
-
-    this.updateAdvice()
+    setInterval(this.fetchAdvice, 60000);
   },
+
+
 
   methods: {
     async fetchAdvice() {
 
-      const options = {
+      let testdate = '2023-06-15T13:59:53';
+
+      let now = new Date().toISOString().split('.')[0];
+
+      console.log('type: ' + typeof (now));
+      console.log('time: ' + now);
+
+      let options = {
         'method': 'GET',
-        'url': 'https://my.deal.by/api/v1/orders/list',
+        'url': 'https://corsproxy.io/?https://my.deal.by/api/v1/orders/list',
         'headers': {
           'Authorization': 'Bearer 09db5f5143554c6b2bc31920a1553be43e7f2c1a',
           "Content-Type": "application/json"
-        }
+        },
+        params: { date_from: now },
       };
 
-      /* const options = {
-        'method': 'GET',
-        'url': 'https://my.deal.by/api/v1/orders/list'
-      }; */
-
-      /* return await Http.request({
-        method: 'GET',
-        url: 'https://api.adviceslip.com/advice',
-
-      }) */
       return await Http.request(
         options
       )
         .then(({ data }) => {
-          // Set dynamic class to fade in text
-          this.animationState = 'fadeIn'
-          this.resetAnimationState()
-
-          // Save new advice
           this.advice = data;
-          // In other words:
-          // const dataInJs = JSON.parse(data)
-          // const slip = dataInJs.slip
-          // this.advice = slip.advice
-          // this.advice = this.advice.toUpperCase() — // font supports upper case only
-
-          // Update lastSaveDate
-          this.lastSaveDate = this.currentDate
         }).catch((error) => {
           this.advice = error;
         });
     },
-
-    updateAdvice() {
-      // If 24h have passed, fetch new advice
-      if (this.currentHour === this.hourToFetchNewAdvice && this.currentDate != this.lastSaveDate) this.fetchAdvice()
-
-      // If 23 hours have passed, start fading out current advice
-      else if (this.currentHour === this.hourToEraseCurrentAdvice && this.advice) {
-        // Set dynamic class to fade out text
-        this.animationState = 'fadeOut'
-        this.resetAnimationState()
-
-        // Clear advice from state after the fade out animation ends
-        setTimeout(() => {
-          this.advice = ''
-        }, 10000)
-      }
-
-      // Check every 10m if it's time to fetch/erase advice
-      setTimeout(this.updateAdvice, 600000)
-    },
-
-    // Clear animation from advice after one playthrough
-    // A safer approach might be to listen for `transistionend`
-    resetAnimationState() {
-      setTimeout(() => {
-        this.animationState = ''
-      }, 10000)
-    }
   },
 });
 </script>
@@ -133,7 +77,7 @@ export default defineComponent({
   background: wheat;
   height: 100%;
   padding: 1rem;
-  
+
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
